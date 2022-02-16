08877cf071db57d720aba84bc978f8eeefd850d9
NumiBlue <thekindlyblog@gmail.com>
2022-02-16 14:14:23 -0600

    functioning short forEach array

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
 const printProfileData = profileDataArr => {
     for (let i = 0; i < profileDataArr.length; i += 1) {
       console.log(profileDataArr[i]);
@@ -7,3 +10,5 @@ const printProfileData = profileDataArr => {
 
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
+
+printProfileData(profileDataArgs);
