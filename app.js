ad2c768307612e2b3b9dcce0c29abe5e6fb22dde
NumiBlue <thekindlyblog@gmail.com>
2022-02-20 12:18:54 -0600

    test

--- app.js
+++ app.js
@@ -1,40 +1,9 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-//console.log(profileDataArgs);
 const fs= require('fs');
-
-
-//const printProfileData = profileDataArr => {
-    //for (let i = 0; i < profileDataArr.length; i += 1) {
-     // console.log(profileDataArr[i]);
-   // }
-
-    //console.log('================');
-
-    //profileDataArr.forEach(profileItem => console.log(profileItem));
-//};
-const generatePage = (name1, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${'Ciara Hargis'}</h1>
-    <h2><a href="https://github.com/${'NumiBlue'}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
-
-//console.log(generatePage('Ciara Hargis', 'NumiBlue'));
-//printProfileData(profileDataArgs);
-
+const generatePage = require('src/pagetemplate.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name1, github] = profileDataArgs;
+
+
 
 fs.writeFile('index.html', generatePage(name1, github), err => {
   if (err) throw err;
